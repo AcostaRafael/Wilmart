2244bfe7cc8c542445b334bf877cda405a137d44
Rafael <acosta15.88@gmail.com>
2019-10-05 20:14:55 -0400

    cambio nuevo

--- js/script.js
+++ js/script.js
@@ -1,83 +1,29 @@
-var button = document.getElementById("enter");
-var input = document.getElementById("userinput");
-const ul = document.querySelector("ul");
-const li = document.getElementsByTagName("li");
-const deleteButton = document.getElementsByTagName("i");
-var clearButton = document.getElementById("clear");
+$(document).ready(function () {
 
-function strikeThrough(e) {
-    if (e.target.tagName === "LI") {
-        e.target.classList.toggle("done");
-    }
-}
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+    });
 
-function createDeleteButtonIcon() {
-    for (var ind = 0; ind < li.length; ind++) {
-        var createDeleteButton = document.createElement("i");
-        var createDiv = document.getElementsByClassName("div");
-        console.log(createDiv);
-        createDeleteButton.classList.add("fa", "fa-trash");
-        createDiv[ind].appendChild(createDeleteButton);
-    }
-}
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
 
-function deleteNodeOnClick(e) {
-    var trash = document.querySelectorAll("i");
-    for (var ind = 0; ind < trash.length; ind++) {
-        console.log(e);
-        this.parentNode.parentNode.remove();
-    }
-}
+});
 
-function inputLength() {
-    return input.value.length;
-}
+$(document).ready(function () {
 
-function createListElement() {
-    var divClassWrapper = document.createElement("div");
-    divClassWrapper.classList.add("li-wrapper");
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+    });
 
-    var li = document.createElement("li");
-    var createDiv = document.createElement("div");
+    $('#sidebarCollapse').on('click', function () {
+        // open or close navbar
+        $('#sidebar').toggleClass('active');
+        // close dropdowns
+        $('.collapse.in').toggleClass('in');
+        // and also adjust aria-expanded attributes we use for the open/closed arrows
+        // in our CSS
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
 
-    li.appendChild(document.createTextNode(input.value));
-    divClassWrapper.appendChild(li);
-    divClassWrapper.appendChild(createDiv);
-    ul.appendChild(divClassWrapper);
-
-    input.value = "";
-    createDiv.classList.add("div");
-    var createDeleteButton = document.createElement("i");
-    createDeleteButton.classList.add("fa", "fa-trash");
-    createDiv.appendChild(createDeleteButton);
-    deleteParentNodeOnClick();
-}
-
-function addListAfterClick() {
-    if (inputLength() > 0) {
-        createListElement();
-    }
-}
-
-function addListAfterKeypress(event) {
-    if (inputLength() > 0 && event.keyCode === 13) {
-        createListElement();
-    }
-}
-
-function deleteParentNodeOnClick() {
-    for (var i = 0; i < deleteButton.length; i++) {
-        deleteButton[i].addEventListener("click", deleteNodeOnClick);
-    }
-}
-
-function clearList() {
-    ul.innerHTML = "";
-}
-
-clearButton.addEventListener("click", clearList);
-ul.addEventListener("click", strikeThrough);
-button.addEventListener("click", addListAfterClick);
-input.addEventListener("keypress", addListAfterKeypress);
-createDeleteButtonIcon();
-deleteParentNodeOnClick();
+});
